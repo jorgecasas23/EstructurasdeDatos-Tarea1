8f04ddd7702dd21b71afa1ef135089ac8ff15e8e
jorgecasas23 <124187999+jorgecasas23@users.noreply.github.com>
2023-05-06 20:08:45 -0500

    Create BigInteger.h

---
+++ proyecto/BigInteger.h
@@ -0,0 +1,18 @@
+/*
+Nombre: Jorge Casas
+Fecha: 28 de abril 2023
+*/
+#include "lista.h"
+
+class bigInteger{
+	Lista();
+	
+	public:
+		void add();
+		void product();
+		void substract();
+		void quotient();
+		void remainder();
+		void pow();
+		int toString();
+};
